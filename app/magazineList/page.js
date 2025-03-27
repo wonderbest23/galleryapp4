b5e8f0173cfb75f76e30c1d5faad8c41fb1dedae
Leejoongjae123 <ljj3347@naver.com>
2025-03-27 22:44:21 +0900

    123

--- app/magazineList/page.js
+++ app/magazineList/page.js
@@ -9,7 +9,6 @@ import Link from "next/link";
 import { createClient } from "@/utils/supabase/client";
 import { useState, useEffect } from "react";
 
-
 export default function MagazineList() {
   const [magazines, setMagazines] = useState([]);
   const [visibleCount, setVisibleCount] = useState(5);
@@ -39,61 +38,75 @@ export default function MagazineList() {
     setVisibleCount(newVisibleCount);
     setAllLoaded(magazines.length <= newVisibleCount);
   };
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center w-full h-screen">
-        <Spinner variant="wave" size="lg" color="danger" />
-      </div>
-    );
-  }
 
   return (
     <div className="flex flex-col items-center justify-center mx-2">
-      <div className="bg-white flex items-center w-[90vw] justify-between">
-        <Button
-          isIconOnly
-          variant="light"
-          className="mr-2"
-          onPress={() => router.back()}
-        >
-          <FaChevronLeft className="text-xl" />
-        </Button>
-        <h2 className="text-lg font-bold text-center flex-grow">매거진</h2>
-        <div className="w-10"></div>
-      </div>
-      <div className="w-full flex flex-col gap-4">
-        {magazines.slice(0, visibleCount).map((item, index) => (
-          <React.Fragment key={item.id}>
-            <Link href={`/magazine/${item.id}`}>
-            <Card className="w-full" isPressable isHoverable shadow="none">
-              <CardBody className="flex gap-4 flex-row justify-between">
-                <div className="flex flex-col space-y-2">
-                  <h3 className="text-sm text-default-500">{item.title}</h3>
-                  <p className="text-lg font-medium">{item.subtitle}</p>
-                  <p className="text-sm text-default-400">{new Date(item.created_at).getFullYear()}년 {new Date(item.created_at).getMonth() + 1}월 {new Date(item.created_at).getDate()}일</p>
-                </div>
-                <Image
-                  alt="Card thumbnail"
-                  className="object-cover w-24 h-24 rounded-lg"
-                  src={item.photo[0].url}
-                />
-              </CardBody>
-            </Card>
-            </Link>
-            {index < visibleCount - 1 && <Divider className="my-2" />}
-          </React.Fragment>
-        ))}
-      </div>
-      <div className="flex flex-col justify-center items-center mt-6 mb-24">
-        {!allLoaded ? (
-          <FaPlusCircle 
-            className="text-red-500 text-2xl font-bold hover:cursor-pointer" 
-            onClick={loadMore}
-          />
-        ) : (
-          <p className="text-gray-500 text-sm">모든 매거진을 불러왔습니다</p>
-        )}
-      </div>
+      {loading ? (
+        <div className="flex items-center justify-center w-full h-screen">
+          <Spinner variant="wave" size="lg" color="danger" />
+        </div>
+      ) : (
+        <>
+          <div className="bg-white flex items-center w-[90vw] justify-between">
+            <Button
+              isIconOnly
+              variant="light"
+              className="mr-2"
+              onPress={() => router.back()}
+            >
+              <FaChevronLeft className="text-xl" />
+            </Button>
+            <h2 className="text-lg font-bold text-center flex-grow">매거진</h2>
+            <div className="w-10"></div>
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            {magazines.slice(0, visibleCount).map((item, index) => (
+              <React.Fragment key={item.id}>
+                <Link href={`/magazine/${item.id}`}>
+                  <Card
+                    className="w-full"
+                    isPressable
+                    isHoverable
+                    shadow="none"
+                  >
+                    <CardBody className="flex gap-4 flex-row justify-between">
+                      <div className="flex flex-col space-y-2">
+                        <h3 className="text-sm text-default-500">
+                          {item.title}
+                        </h3>
+                        <p className="text-lg font-medium">{item.subtitle}</p>
+                        <p className="text-sm text-default-400">
+                          {new Date(item.created_at).getFullYear()}년{" "}
+                          {new Date(item.created_at).getMonth() + 1}월{" "}
+                          {new Date(item.created_at).getDate()}일
+                        </p>
+                      </div>
+                      <Image
+                        alt="Card thumbnail"
+                        className="object-cover w-24 h-24 rounded-lg"
+                        src={item.photo[0].url}
+                      />
+                    </CardBody>
+                  </Card>
+                </Link>
+                {index < visibleCount - 1 && <Divider className="my-2" />}
+              </React.Fragment>
+            ))}
+          </div>
+          <div className="flex flex-col justify-center items-center mt-6 mb-24">
+            {!allLoaded ? (
+              <FaPlusCircle
+                className="text-red-500 text-2xl font-bold hover:cursor-pointer"
+                onClick={loadMore}
+              />
+            ) : (
+              <p className="text-gray-500 text-sm">
+                모든 매거진을 불러왔습니다
+              </p>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
