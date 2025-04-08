3f83b9879bb36c88ba1999c328274bbe716ce758
Leejoongjae123 <ljj3347@naver.com>
2025-04-08 20:06:48 +0900

    123

--- app/(client)/components/BottomNavigationbar.js
+++ app/(client)/components/BottomNavigationbar.js
@@ -21,7 +21,7 @@ export default function BottomNavigation() {
           </svg>
           <span className="text-xs mt-1">리스트</span>
         </Link>
-        <Link href="/exhibitionList?isBookmark=true" className={`flex flex-col items-center justify-center w-1/4 h-full ${pathname.startsWith('/exhibitionList') ? 'text-red-500' : 'text-gray-500 hover:text-red-500'}`}>
+        <Link href="/exhibitions?isBookmark=true" className={`flex flex-col items-center justify-center w-1/4 h-full ${pathname.startsWith('/exhibitionList') ? 'text-red-500' : 'text-gray-500 hover:text-red-500'}`}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
           </svg>
