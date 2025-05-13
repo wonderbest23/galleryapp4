cf383835824467bc123dedbf4b7013ad372ba7c6
ljj <ljj3347@naver.com>
2025-05-13 10:23:55 +0900

    add noti

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -170,6 +170,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      
 
       {/* 이용약관 모달 */}
       <Modal className="h-full max-h-[75vh] overflow-y-auto " size="2xl" isOpen={termsDisclosure.isOpen} onClose={termsDisclosure.onClose}>
