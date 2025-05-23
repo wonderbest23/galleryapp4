0628dc2c35902740eefa33bfebecb93e84c9f754
wonderbest23 <168019437+wonderbest23@users.noreply.github.com>
2025-05-23 10:34:08 +0900

    Update Froala.js
    에러수정

--- app/(admin)/admin/components/Froala.js
+++ app/(admin)/admin/components/Froala.js
@@ -294,10 +294,7 @@ const FroalaEditorComponent = ({
   };
 
   return (
-    <div className="froala-editor-container col-span-2
-     w-[390px]    
-     sm:w-full
-     mx-auto
+     <div className="froala-editor-container col-span-2 w-[390px] sm:w-full mx-auto">
       {froalaError ? (
         <p>에디터를 불러오는 데 실패했습니다. 새로고침 해주세요.</p>
       ) : editorReady && FroalaEditor && typeof FroalaEditor === 'function' ? (
